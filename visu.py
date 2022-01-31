db92b7c1ca7dca31405eff44f1834fbdf307cd5a
AdamGouja <adam.gouja@gmail.com>
2022-01-31 18:22:23 +0100

    first graph

--- visu.py
+++ visu.py
@@ -4,35 +4,60 @@ from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output, State
 
+import plotly.express as px
+import pandas as pd
+import pymongo
+from pymongo import MongoClient
+from datetime import datetime
+
 dict_LFL =  [
-    {'label' : 'GamersOrigin', 'value':'GO'},
-    {'label' : 'GameWard', 'value':'GW'},
-    {'label' : 'Karmine Corp', 'value':'KC'},
-    {'label' : 'LDLC OL', 'value':'LDLC'},
-    {'label' : 'Mirage Elyandra', 'value':'ME'},
-    {'label' : 'Misfits Premier', 'value':'MSFP'},
-    {'label' : 'Solary', 'value':'SLY'},
-    {'label' : 'Team BDS Academy', 'value':'BDS'},
-    {'label' : 'Team Oplon', 'value':'OPL'},
-    {'label' : 'Vitality.Bee', 'value':'VITB'}
+    {'label' : 'GamersOrigin', 'value':'Gamers Origin'},
+    {'label' : 'GameWard', 'value':'Gameward'},
+    {'label' : 'Karmine Corp', 'value':'KCORP'},
+    {'label' : 'LDLC OL', 'value':'LDLC OL'},
+    {'label' : 'Mirage Elyandra', 'value':'Mirage Elyandra'},
+    {'label' : 'Misfits Premier', 'value':'Misfits Academy'},
+    {'label' : 'Solary', 'value':'Solary'},
+    {'label' : 'Team BDS Academy', 'value':'BDS Academy'},
+    {'label' : 'Team Oplon', 'value':'Team Oplon'},
+    {'label' : 'Vitality.Bee', 'value':'Vitality Bee'}
 ]
 
 dict_LEC =  [
-    {'label' : 'Astralis', 'value':'AST'},
-    {'label' : 'Excel', 'value':'XL'},
-    {'label' : 'Fnatic', 'value':'FNC'},
+    {'label' : 'Astralis', 'value':'Astralis'},
+    {'label' : 'Excel', 'value':'Excel'},
+    {'label' : 'Fnatic', 'value':'Fnatic'},
     {'label' : 'G2 Esports', 'value':'G2'},
-    {'label' : 'MAD Lions', 'value':'MAD'},
-    {'label' : 'Misfits Gaming', 'value':'MSF'},
-    {'label' : 'Rogue', 'value':'RGE'},
-    {'label' : 'SK Gaming', 'value':'SK'},
-    {'label' : 'Team BDS', 'value':'BSD'},
-    {'label' : 'Team Vitality', 'value':'VIT'}
+    {'label' : 'MAD Lions', 'value':'Mad Lions'},
+    {'label' : 'Misfits Gaming', 'value':'Misfits'},
+    {'label' : 'Rogue', 'value':'Rogue'},
+    {'label' : 'SK Gaming', 'value':'SK Gaming'},
+    {'label' : 'Team BDS', 'value':'BDS'},
+    {'label' : 'Team Vitality', 'value':'Vitality'}
 ]
+list_all_values =  ['Gamers Origin','Gameward','KCORP','LDLC OL','Miage Elyandra','Misfits Academy','Solary','BDS Academy','Team Oplon','Vitality Bee',
+'Astralis','Excel','Fnatic','G2','Mad Lions','Misfits','Rogue','SK Gaming','BDS','Vitality']
+
+dict_all = dict_LFL+dict_LEC
+
+client = MongoClient('localhost',27017)
+db = client.project
+
+all_data = db['all_data']
+tag = db['tweets_tag']
+
+query = {'$or': [{'tag': 'KCORP'}]}
 
 if __name__ == '__main__':
 
     app = dash.Dash(__name__)
+
+    tweets_date = px.line(
+        pd.DataFrame(list(tag.find(query))),
+        x='date',
+        y='count',
+        color='tag'
+    )
 
     app.layout = html.Div(
         children=[
@@ -63,7 +88,8 @@ if __name__ == '__main__':
                                 id='league_choice',
                                 options = [
                                     {'label' : 'Ligue Française (LFL)', 'value' : 'LFL'},
-                                    {'label' : 'Ligue Européenne (LEC)', 'value' : 'LEC'}
+                                    {'label' : 'Ligue Européenne (LEC)', 'value' : 'LEC'},
+                                    {'label' : 'LEC & LFL', 'value' : 'all'}
                                 ],
                                 clearable=False,
                                 value = 'LFL'
@@ -76,13 +102,22 @@ if __name__ == '__main__':
                         id='date_range_div',
                         children=[
                             html.H3(children='Date range'),
-                            dcc.RangeSlider(
+                            dcc.DatePickerRange(
                                 id='date_range',
-                                marks={-1:'14/11/2021',0:'21/12/2021',1:'12/01/2021',2:'30/01/2021'},
-                                min=-1,
-                                max=2,
-                                value = [-1,2]
-                            )
+                                min_date_allowed=datetime.strptime('2021-11-14', "%Y-%m-%d").date(),
+                                max_date_allowed=datetime.strptime('2022-01-30', "%Y-%m-%d").date(),
+                                initial_visible_month=datetime.strptime('2021-11-14', "%Y-%m-%d").date(),
+                                end_date=datetime.strptime('2021-01-30', "%Y-%m-%d").date(),
+                                start_date=datetime.strptime('2021-11-14', "%Y-%m-%d").date(),
+                            ),
+                            # dcc.RangeSlider(
+                            #     id='date_range',
+                            #     marks={'14/11/2021','21/12/2021','12/01/2021','30/01/2021'},
+                            #     # min=-1,
+                            #     # max=2,
+                            #     tooltip={"placement": "bottom", "always_visible": True},
+                            #     value = [-1,2]
+                            # )
                         ],
                         hidden = False
                     ),
@@ -93,16 +128,17 @@ if __name__ == '__main__':
                             html.H3(
                                 children='Team(s) Choice'
                             ),
-                            dcc.Checklist(
-                                id="check_all",
-                                options=[{"label": "Check/Uncheck All", "value": "All"}],
-                                value=[],
-                                labelStyle={"display": "inline-block"},
-                            ),
-                            html.H3(),
+                            # dcc.Checklist(
+                            #     id="check_all",
+                            #     options=[{"label": "Check/Uncheck All", "value": "All"}],
+                            #     value=[],
+                            #     labelStyle={"display": "inline-block"},
+                            # ),
+                            # html.H3(),
                             dcc.Checklist(
                                 id='check_team',
                                 options = dict_LFL,
+                                # value = ["KCORP"],
                                 labelStyle={'display': 'block'}
                             ) 
                         ],
@@ -122,8 +158,8 @@ if __name__ == '__main__':
 
                     html.Div(
                         id='figure_display',
-                        children = dcc.Graph(id='graph_figure'),
-                        hidden=True
+                        children = dcc.Graph(id='graph_figure',figure=tweets_date),
+                        hidden=False
                     ),
 
                     html.Div(
@@ -135,7 +171,7 @@ if __name__ == '__main__':
                             height='400px',
                             width='100%'
                         ),
-                        hidden=False
+                        hidden=True
                     ),
                     
                 ],
@@ -147,31 +183,33 @@ if __name__ == '__main__':
 
     ###### CALLBACK
 
-    @app.callback(
-        Output(component_id="check_team", component_property="value"),
-        [Input(component_id="check_all", component_property="value")],
-        [State(component_id="check_team", component_property="options")],
-    )
-    def select_all_none(all_selected, options):
-        """
-        Select or unselect all the choices in the team choices.
-        """
-        all_or_none = []
-        all_or_none = [option["value"] for option in options if all_selected]
-        return all_or_none
+    # @app.callback(
+    #     Output(component_id="check_team", component_property="value"),
+    #     [Input(component_id="check_all", component_property="value")],
+    #     [State(component_id="check_team", component_property="options")],
+    # )
+    # def select_all_none(all_selected, options):
+    #     """
+    #     Select or unselect all the choices in the team choices.
+    #     """
+    #     all_or_none = []
+    #     all_or_none = [option["value"] for option in options if all_selected]
+    #     return all_or_none
 
     @app.callback(
         Output(component_id="check_team", component_property="options"),
         [Input(component_id="league_choice", component_property="value")]
     )
-    def change_team(input_value):
+    def change_teams(input_value):
         """
         Select or unselect all the choices in the team choices.
         """
         if input_value == 'LFL':
             return dict_LFL
-        else:
+        elif input_value == 'LEC':
             return dict_LEC
+        else :
+            return dict_all
 
     @app.callback(
         [Output(component_id="check_team_div", component_property="hidden"),
@@ -183,5 +221,31 @@ if __name__ == '__main__':
             return [True, True]
         else :
             return [False, False]
+
+    @app.callback(
+        Output(component_id="graph_figure", component_property="figure"),
+        [Input(component_id="check_team", component_property="value"),
+        Input(component_id="date_range", component_property="start_date"),
+        Input(component_id="date_range", component_property="end_date")]
+    )
+    def graph_tweet(team, start, end):
+        end_date = end
+        start_date = start
+        if not team:
+            team = list_all_values
+        tags = team
+        liste = []
+        dic = {}
+        for i in tags:
+            dic['tag'] = i
+            liste.append(dic.copy())
+        Query = {'date':{'$gte':start_date,'$lt':end_date},"$or" : liste}
+        return px.line(
+            pd.DataFrame(list(tag.find(Query))),
+            x='date',
+            y='count',
+            color='tag'
+        )
+
 
     app.run_server(debug=True)
